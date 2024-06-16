e0839c62fd1f47474d2e15df23cdd88ceb8225f5
marufhossain55 <engrmaruf3@gmail.com>
2024-06-16 09:44:37 +0600

    middleware end

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+////////////////////
+const port = process.env.PORT || 5000;
+//<----middleware--->
+const coresOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+//............................//\
+app.use(cors(coresOptions));
+app.use(express.json());
+app.use(cookieParser());
+//<---------------middleware--------------->//
