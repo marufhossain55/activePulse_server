5e02b89695c1d4f03a18193231facc99e3caf9c5
marufhossain55 <engrmaruf3@gmail.com>
2024-06-21 09:51:29 +0600

    mongodb connection added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 ////////////////////
 const port = process.env.PORT || 5000;
 //<----middleware--->
@@ -14,7 +15,38 @@ const coresOptions = {
 app.use(cors(coresOptions));
 app.use(express.json());
 // app.use(cookieParser());
-//<---------------middleware--------------->//
+//<---------------middleware END--------------->//
+
+//<----------mongoDb----------->
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.va5jejf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//<-------------mongoDb end------------->
 
 app.get('/', (req, res) => {
   res.send('ActivePulse is running');
@@ -23,3 +55,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`active pulse is running on port ${port}`);
 });
+
+/**
+ * --------------------------------
+ *      NAMING CONVENTION
+ * --------------------------------
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('/users/:id')
+ * app.delete('/users/:id')
+ *
+ */
