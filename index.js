7b70005bc60f817d5cdceb741064bbf3ebd2b376
marufhossain55 <engrmaruf3@gmail.com>
2024-06-22 16:20:52 +0600

    verifytoken updated

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'forbidden access' });
+        return res.status(401).send({ message: 'unauthorize access' });
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
